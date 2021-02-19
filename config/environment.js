dc35617c0f71ee45dd68755c822063951d76d41d
Kedar Kulkarni <kedarvijaykulkarni@gmail.com>
2021-02-19 19:04:45 +0530

    developer console enable

--- config/environment.js
+++ config/environment.js
@@ -26,9 +26,12 @@ module.exports = function(environment) {
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
+    ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.RAISE_ON_DEPRECATION = true;
+    ENV.LOG_STACKTRACE_ON_DEPRECATION = true;
+    ENV.SHOW_ERROR_CONSOLE_LOGS = true;
   }
 
   if (environment === 'test') {
