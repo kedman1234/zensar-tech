466a8a30b9bd0111e99016ce9d5a400bfde15916
Kedar Kulkarni <kedarvijaykulkarni@gmail.com>
2021-02-20 01:56:22 +0530

    assignment complete

--- app/list/route.js
+++ app/list/route.js
@@ -1,48 +1,28 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
   model() {
-    return [
-      {
-        name: 'Young Gatchell',
-        account: '@yg',
-        img: 'http://mrmrs.github.io/photos/p/2.jpg'
-      },
-      {
-        name: 'Arnoldo Degraff',
-        account: '@99xx88randomhandle',
-        img: 'http://mrmrs.github.io/photos/p/3.jpg'
-      },
-      {
-        name: 'Deirdre Lachance',
-        account: '@@favfavfav',
-        img: 'http://mrmrs.github.io/photos/p/4.jpg'
-      },
-      {
-        name: 'Frederic Starner',
-        account: '@hungryhippofanatic',
-        img: 'http://mrmrs.github.io/photos/p/5.jpg'
-      },
-      {
-        name: 'Cleveland Ridout',
-        account: '@purethinking',
-        img: 'http://mrmrs.github.io/photos/p/6.jpg'
-      },
-      {
-        name: 'Leticia Fearon',
-        account: '@retrofeels',
-        img: 'http://mrmrs.github.io/photos/p/7.jpg'
-      },
-      {
-        name: 'Ahmad Backer',
-        account: '@ahmadBB',
-        img: 'http://mrmrs.github.io/photos/p/10.jpg'
-      },
-      {
-        name: 'Carlie Noakes',
-        account: '@carnoakes99',
-        img: 'http://mrmrs.github.io/photos/p/11.jpg'
-      }
-    ]
+   return fetch('./products.json')
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      let productCategory = data.map((value)=>{
+        return value.productCategory
+      }).filter((value, index, self) => {
+        return self.indexOf(value) === index;
+      })
+      return RSVP.hash({
+        productCategory,
+        data: data
+      })
+      
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log('err', err)
+      // Do something for an error here
+    })
   }
 });
